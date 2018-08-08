6674005900bbca36a496faaa211792720aad34ea
徐远翔 <xuyuanxiang@wosai-inc.com>
2018-08-08 16:47:12 +0800

    done

--- packages/get-chiron/index.js
+++ packages/get-chiron/index.js
@@ -1,2 +1,27 @@
 #!/usr/bin/env node
-require('./lib');
+const { execSync } = require('child_process');
+const { dirname } = require('path');
+
+try {
+  tryYarn();
+} catch (e) {
+  execSync('npm install -g chiron-cli', {
+    stdio: 'inherit',
+    encoding: 'utf8',
+  });
+}
+
+function tryYarn() {
+  const bin = execSync('yarn global bin', {
+    stdio: 'pipe',
+    encoding: 'utf8',
+  });
+  const prefix = dirname(bin);
+  execSync(
+    `yarn config set prefix ${prefix} && yarn global add chiron-cli --prefix ${prefix}`,
+    {
+      stdio: 'inherit',
+      encoding: 'utf8',
+    },
+  );
+}
