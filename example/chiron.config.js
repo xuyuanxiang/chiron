d203c5950fb323f937a2c34ef331f3c4632312bf
我心爱的OptiPlex-7060 <xuyuanxiang@shouqianba.com>
2018-10-18 11:38:10 +0800

    example

--- example/chiron.config.js
+++ example/chiron.config.js
@@ -1,11 +1,14 @@
+const AliyunOSSPlugin = require('aliyun-oss-webpack-plugin');
+const { name, version } = require('./package.json');
 module.exports = {
   webpack(config) {
     /**
-     * add @babel/polyfill
+     * custom config
      * @see webpack-chain
      */
-    config.when(config.get('mode') === 'production', config => {
+    config.when(config.get('mode') === 'production', config =>
       config
+        // add @babel/polyfill
         .entry('main')
         .prepend('@babel/polyfill')
         .end()
@@ -17,7 +20,20 @@ module.exports = {
             ['@babel/preset-env', { useBuiltIns: 'entry', targets: '> 0.25% in CN' }],
             ...options.presets.slice(1),
           ],
-        }));
-    });
+        }))
+        .end()
+        .end()
+        .end()
+        .output.publicPath(`https://yours.domain/${name}/${version}/`)
+        .plugin('aliyun-oss')
+        .use(AliyunOSSPlugin, [
+          {
+            region: 'oss-cn-hangzhou',
+            accessKeyId: '***',
+            accessKeySecret: '***',
+            bucket: 'your bucket',
+          },
+        ]),
+    );
   },
 };
