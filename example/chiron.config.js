de97656cbd54ffe5050104eff245737d0179f2c6
我心爱的OptiPlex-7060 <xuyuanxiang@shouqianba.com>
2018-10-18 11:18:28 +0800

    done

--- example/chiron.config.js
+++ example/chiron.config.js
@@ -1,5 +1,23 @@
 module.exports = {
   webpack(config) {
-    console.log('webpack config');
+    /**
+     * add @babel/polyfill
+     * @see webpack-chain
+     */
+    config.when(config.get('mode') === 'production', config => {
+      config
+        .entry('main')
+        .prepend('@babel/polyfill')
+        .end()
+        .module.rule(global.__CHIRON_RULE_JS__)
+        .use(global.__CHIRON_LOADER_BABEL__)
+        .tap(options => ({
+          ...options,
+          presets: [
+            ['@babel/preset-env', { useBuiltIns: 'entry', targets: '> 0.25% in CN' }],
+            ...options.presets.slice(1),
+          ],
+        }));
+    });
   },
 };
