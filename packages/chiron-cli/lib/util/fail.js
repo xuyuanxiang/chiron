c714a38ba46809ed7b18a62726cfda61536aa40d
我心爱的OptiPlex-7060 <xuyuanxiang@shouqianba.com>
2018-10-18 11:21:03 +0800

    remove lib

--- packages/chiron-cli/lib/util/fail.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1(info) {
-    console.error(info);
-    process.exit(1);
-}
-exports.default = default_1;
